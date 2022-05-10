cf33dd897f260b473ec34f7bd8b7e42fb0d35332
Maddy Ali <maddyoali@gmail.com>
2022-05-10 07:48:52 -0500

    Add dynamic grid that updates based on user input

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const grid = document.getElementById("grid");
+const sizeSlider = document.getElementById("sizeSlider");
+const sizeValue = document.getElementById("sizeValue");
+const clearBtn = document.getElementById("clearBtn");
+
+// global variable
+let currentSize = 16;
 
 function createGrid(size) {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -11,10 +17,28 @@ function createGrid(size) {
     grid.appendChild(gridItem);
   }
 }
-createGrid(16);
+createGrid(currentSize);
 
 function changeColor(e) {
   if (e.target.matches(".grid-item")) {
     e.target.classList.add("active");
   }
+}
+sizeSlider.addEventListener("input", (e) => updateSizeValue(e.target.value));
+sizeSlider.addEventListener("input", (e) => changeGridSize(e.target.value));
+clearBtn.onclick = reloadGrid;
+
+function updateSizeValue(value) {
+  sizeValue.textContent = `${value} x ${value}`;
+}
+function clearGrid() {
+  grid.textContent = "";
+}
+function reloadGrid() {
+  clearGrid();
+  createGrid(currentSize);
+}
+function changeGridSize(value) {
+  currentSize = value;
+  reloadGrid();
 }
